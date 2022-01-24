5aaa168fac876122847c44f42bdc70505e2bb15a
Jared Lewis <jlewis8493@gmail.com>
2022-01-24 17:58:16 -0500

    Exercise 1 TODOs complete

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Create an object of your Bird class
+             *  give values to your members using the object of your Bird class
+             *  
+             * Creatively display the class member values 
+             */
+
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
+
+
+            var lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "swamp",
+                CanGrowTail = true
+            };
+
+            var myAnimals = new Animal[] { myBird, lizard };
+
+            foreach (var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age} years old");
+                Console.WriteLine($"It has {animal.LegCount} legs");
+                Console.WriteLine($"It lives by {animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
+            
+        }
+    }
+}
